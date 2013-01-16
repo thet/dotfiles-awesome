4b4877efa130df8c8e80fc31b182948cf82fb1a8
Johannes Raggam <raggam-nl@adm.at>
2013-01-16 18:23:24 +0100

    force no maximized on startup. see: https://awesome.naquadah.org/wiki/FAQ#Why_some_floating_clients_can_not_be_tiled.3F

--- windowrules.lua
+++ windowrules.lua
@@ -4,7 +4,9 @@ awful.rules.rules = {
                      border_color = beautiful.border_normal,
                      focus = true,
                      keys = clientkeys,
-                     buttons = clientbuttons } },
+                     buttons = clientbuttons,
+                     maximized_vertical   = false, -- force no maximized on startup. see: https://awesome.naquadah.org/wiki/FAQ#Why_some_floating_clients_can_not_be_tiled.3F
+                     maximized_horizontal = false } },
     { rule = { class = "MPlayer" },
       properties = { floating = true } },
     { rule = { class = "pinentry" },
